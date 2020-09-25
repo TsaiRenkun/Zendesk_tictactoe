95ab281b7ae65acc29a7829f83048ac70b334d2f
Tsairenkun <Tsairenkun@gmail.com>
2020-09-25 15:47:14 +0800

    Creating the layout and the board for the game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import Square from './Square';
+import React from "react";
+import Square from "./Square";
 
-export const Board = () => {
-    return (
-        <div className = "board">
-            
-        </div>
-    )
-}
-
+export const Board = (arr, onClick) => {
+  return (
+    <div className="board">
+      {arr.map((val, i) => (
+        <Square
+          key={i}
+          value={val}
+          onClick={() => {
+            onClick[i];
+          }}
+        />
+      ))}
+    </div>
+  );
+};
---
+++ src/components/Game.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import Board from './Board';
+
+export const Game = () => {
+    return (
+        <div>
+            <h1>React Tic Tac Toe</h1>
+        </div>
+    )
+}
