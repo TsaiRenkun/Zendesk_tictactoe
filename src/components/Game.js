b09bf8493f2925ce3e317826c1729569ae9d62c3
Tsairenkun <Tsairenkun@gmail.com>
2020-09-25 15:48:43 +0800

    Create Array for 3 x 3 tic tac toe

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 import Board from './Board';
 
 export const Game = () => {
+
+    const [board, SetBoard] = useState([Array(9).full(null)])
+
     return (
         <div>
             <h1>React Tic Tac Toe</h1>
